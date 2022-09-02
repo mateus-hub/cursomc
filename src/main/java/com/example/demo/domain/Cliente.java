414f0f28cebaec63bbe1279b4585fcacfb2ef482
mateus-hub <theusskt2018@gmail.com>
2022-09-01 22:17:04 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/example/demo/domain/Cliente.java
+++ src/main/java/com/example/demo/domain/Cliente.java
@@ -22,7 +22,6 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 public class Cliente implements Serializable {
 	private static final long serialVersionUID = 1L;
 
-	
 	@Id
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
@@ -40,6 +39,10 @@ public class Cliente implements Serializable {
 	@ElementCollection
 	@CollectionTable(name="TELEFONE")
 	private Set<String> telefones = new HashSet<>();
+	
+	
+	@OneToMany(mappedBy="cliente")
+	private List<Pedido> pedidos = new ArrayList<>();
 	
 	public Cliente() {
 	}
@@ -107,6 +110,14 @@ public class Cliente implements Serializable {
 
 	public void setTelefones(Set<String> telefones) {
 		this.telefones = telefones;
+	}
+	
+	public List<Pedido> getPedidos() {
+		return pedidos;
+	}
+
+	public void setPedidos(List<Pedido> pedidos) {
+		this.pedidos = pedidos;
 	}
 
 	@Override
